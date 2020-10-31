fbfa5db1e4c59528bfcb304d40fc9587f2e9a417
btRooke <rookebilly@gmail.com>
2020-10-31 15:14:43 +0000

    Frontend

--- TokenRingOfFireFrontEnd/js/guiManipulation.js
+++ TokenRingOfFireFrontEnd/js/guiManipulation.js
@@ -1,4 +1,45 @@
 
+const cardPath = "/cards";
+
+
+function showStartPage() {
+
+    showClearTitlePage();
+    let mainGrid = document.getElementById("highest-grid");
+    mainGrid.innerHTML += `<div class="join-create-container"><input type="text" placeholder="Username"><button onclick="showEnterGameCodeScreen()"> Join Game </button><button> Create Game </button></div>;`
+
+}
+
+function showEnterGameCodeScreen() {
+    showClearTitlePage();
+    let mainGrid = document.getElementById("highest-grid");
+    mainGrid.innerHTML += `<div class="join-create-container"><input type="text" placeholder="Enter Game Code"><button> Submit</button></div>`
+}
+
+function showMainPage() {
+
+}
+
+function showClearTitlePage() {
+    let mainGrid = document.getElementById("highest-grid");
+    mainGrid.innerHTML = "";
+    mainGrid.appendChild(generateTitleElement());
+}
+
+function generateTitleElement() {
+    
+    let titleElement = document.createElement("div");
+    titleElement.setAttribute("class", "title-grid-element");
+
+    let titleTitle = document.createElement("div");
+    titleTitle.setAttribute("class", "main-title");
+    titleTitle.innerHTML = "Token Ring of Fire";
+    titleElement.appendChild(titleTitle);
+
+    return titleElement;
+}
+
+
 function showCard(cardPath) {
     let mainElement = document.getElementById("main");
     mainElement.innerHTML = "";
